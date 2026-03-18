fcae30748a14813915c5eceb6e71831190f5d9f2
nikithasinde13 <nikitha.sinde@gmail.com>
2026-03-18 08:39:16 +0000

    day-25

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,36 +1,13 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-// import { AppComponent } from './app.component';
-// import { AppRoutingModule } from './app-routing.module';  
-// import { AuthInterceptor } from './auth.interceptors';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     HttpClientModule,
-//     AppRoutingModule 
-//   ],
-//   providers: [
-//     {
-//       provide: HTTP_INTERCEPTORS,
-//       useClass: AuthInterceptor,
-//       multi: true
-//     }
-//   ],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule {}
-
-
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+// ⭐ IMPORTANT: SharedModule contains NavbarComponent
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -38,9 +15,13 @@ import { AppRoutingModule } from './app-routing.module';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule       // ⭐ REQUIRED for <app-navbar> to work
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
--- client/src/app/shared/navbar/navbar.component.ts
+++ client/src/app/shared/navbar/navbar.component.ts
@@ -1,25 +1,3 @@
-
-
-// import { Component, OnInit } from '@angular/core';
-
-// @Component({
-//   selector: 'app-navbar',
-//   templateUrl: './navbar.component.html',
-//   styleUrls: ['./navbar.component.scss']
-// })
-// export class NavBarComponent implements OnInit {
-
-//   // ✅ FIX: initialize property
-//   role: string | null = null;
-
-//   constructor() {}
-
-//   ngOnInit(): void {
-//     console.log(localStorage.getItem('role'));
-//     this.role = localStorage.getItem('role');
-//   }
-// }
-
 import { Component, OnInit } from '@angular/core';
 
 @Component({
